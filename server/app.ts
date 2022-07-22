5d0aa4b18f8ae350231bd9b663491fec6bd10243
Gaurav <gaurav.r@arbyte.com.np>
2022-07-22 22:53:41 +0545

    |LOTS OF LITTLE THINGS SET UP |

---
+++ server/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import next from 'next';
+import { env } from './dotenv';
+
+const dev = process.env.NODE_ENV !== 'production';
+const hostname = 'localhost';
+
+// when using middleware `hostname` and `port` must be provided below
+const nextApp = next({ dev, hostname, port: env.PORT });
+const handle = nextApp.getRequestHandler();
+const expressApp = express();
+nextApp.prepare().then(() => {
+  console.log('it works');
+
+  expressApp.use('/api', (req, res, next) => {
+    res.send('THIS IS ROOT API');
+  });
+
+  expressApp.get('*', async (req, res, next) => {
+    console.log(req.url);
+    await handle(req, res, req.originalUrl as any);
+  });
+});
+export { expressApp, nextApp };
--- server/index.ts
+++ server/index.ts
@@ -1,27 +1,5 @@
-import { Client } from 'discord.js';
+import { expressApp } from './app';
 import { env } from './dotenv';
-
-const client = new Client({ intents: 'Guilds' });
-client.once('ready', () => {
-  console.log('Ready!');
+expressApp.listen(env.PORT, () => {
+  console.info(`The server is now running at port ${env.PORT}`);
 });
-
-client.on('interactionCreate', async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-  console.log(interaction.options.getSubcommand());
-  const { options, commandName } = interaction;
-
-  if (commandName === 'ping') {
-    await interaction.reply('Pong!');
-  } else if (commandName === 'server') {
-    await interaction.reply(
-      `Server name: ${interaction.guild?.name}\nTotal members: ${interaction.guild?.memberCount}`,
-    );
-  } else if (options.getSubcommand() === 'name') {
-    await interaction.reply(`Server name ${interaction.guild?.name}`);
-  }
-});
-
-client.login(env.BOT_TOKEN);
-
-console.log('IT WORKSS');
